5f4d356dfc7f3f76e9202886db9f43f3969801cd
MagMigo <magmigo2@inbox.ru>
2022-05-28 12:14:00 +0800

    XD

--- DiscordActivity/activity.py
+++ DiscordActivity/activity.py
@@ -18,14 +18,14 @@ class Activity:
     def __init__(self, bot):
         self.__token = bot.http.token
         self.__config = Config()
-    
-    async def send_activity(self, voice: disnake.VoiceChannel, name: str):
-        requester = Request(
+        self.requester = Request(
             activities_config=self.__config,
             request_data={
-                "method": "POST", 
-                "url": f"{self.config.BASE_URl}/channels/{voice.id}/invites",
+                "method": "POST",
                 "headers": {'Authorization': f'Bot {self.__token}', 'Content-Type': 'application/json'}
             }
         )
-        return await requester.post_request(_id=APPS[name]) 
+    
+    async def send_activity(self, voice: disnake.VoiceChannel, name: str):
+        requester = self.requester.update(url=f"{self.config.BASE_URl}/channels/{voice.id}/invites")
+        return await requester.post_request(_id=APPS[name])
